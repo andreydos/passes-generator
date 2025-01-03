114a2c0b3989df49b89e4b7bf2fd33495e987781
ivanshuba <ivanshuba232@gmail.com>
2025-01-03 16:24:42 +0200

    Change POST method to GET and rename "create" endpoint to "pass"

---
+++ src/passes/passModels/models/transportPass.ts
@@ -0,0 +1,57 @@
+import {format} from "date-fns/format";
+import {PassQueryParams, PassTypeEnum} from "../../passes.types";
+
+export const getTransportPass = (data: PassQueryParams) => {
+  const startDate = data.type === PassTypeEnum.TRANSPORT_SUBSCRIPTION
+    ? format(data.startDate, 'dd-M-y')
+    : format(data.startDate, 'dd-M-y HH:mm');
+
+  const endDate = data.type === PassTypeEnum.TRANSPORT_SUBSCRIPTION
+    ? format(data.endDate, 'dd-M-y')
+    : format(data.endDate, 'dd-M-y HH:mm');
+
+  return {
+    logoText: data.typeName.toUpperCase(),
+      expirationDate: new Date(data.endDate).toISOString(),
+    generic: {
+    headerFields: [{
+      "key": "subscriptionType",
+      "label": "Тип",
+      "value": `${data.typePeriod}`,
+    }],
+    primaryFields: [
+      {
+        "key": "endDate",
+        "label": "Дійсний до",
+        "value": endDate,
+      }
+    ],
+      "secondaryFields": [
+      {
+        "key": "number",
+        "label": "Номер",
+        "value": data.number,
+      },
+      {
+        "key": "startDate",
+        "label": "Дата придбання",
+        "value": startDate,
+        "textAlignment": "PKTextAlignmentRight"
+      }
+    ],
+      "auxiliaryFields": [
+      {
+        "key": "price",
+        "label": "Вартість",
+        "value": `${data.price} UAH`
+      },
+      {
+        "key": "organization",
+        "label": "Організація",
+        "value": "КП Дніпровський ЕДМР",
+        "textAlignment": "PKTextAlignmentRight"
+      }
+    ]
+  }
+  }
+};
--- src/passes/passes.types.ts
+++ src/passes/passes.types.ts
@@ -1,11 +1,14 @@
-export type CreatePassBody = {
-  type: 'SUBSCRIPTION',
-  data: SubscriptionPassCreateData;
+export type PassQueryParams = {
+  type: PassTypeEnum;
+} & TransportPassData;
+
+export enum PassTypeEnum {
+  TRANSPORT_SUBSCRIPTION = 'TRANSPORT_SUBSCRIPTION',
+  TRANSPORT_TICKET = 'TRANSPORT_TICKET',
 }
 
-export type SubscriptionPassCreateData = {
-  id: string,
-  type: string;
+export type TransportPassData = {
+  id: string;
   startDate: string;
   endDate: string;
   number: string;
